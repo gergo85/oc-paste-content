811510c5b7314ad32282d0b4b6844c99cb936b16
Szabó Gergő <gergo85@gmail.com>
2018-09-24 09:34:11 +0200

    Added multilanguage support

--- models/Block.php
+++ models/Block.php
@@ -6,12 +6,22 @@ class Block extends Model
 {
     use \October\Rain\Database\Traits\Validation;
 
+    public $implement = ['@RainLab.Translate.Behaviors.TranslatableModel'];
+
     protected $table = 'indikator_paste_block';
 
     public $rules = [
         'name'   => 'required',
         'code'   => ['required', 'regex:/^[a-z0-9\/\:_\-\*\[\]\+\?\|]*$/i', 'unique:indikator_paste_block'],
         'status' => 'required|between:1,2|numeric'
+    ];
+
+    public $translatable = [
+        'title',
+        'subtitle',
+        'content',
+        'button_name',
+        'button_link'
     ];
 
     public function getCategoryOptions()
--- models/Text.php
+++ models/Text.php
@@ -6,6 +6,8 @@ class Text extends Model
 {
     use \October\Rain\Database\Traits\Validation;
 
+    public $implement = ['@RainLab.Translate.Behaviors.TranslatableModel'];
+
     protected $table = 'indikator_paste_text';
 
     public $rules = [
@@ -13,5 +15,9 @@ class Text extends Model
         'code'   => ['required', 'regex:/^[a-z0-9\/\:_\-\*\[\]\+\?\|]*$/i', 'unique:indikator_paste_text'],
         'type'   => 'required|between:1,3|numeric',
         'status' => 'required|between:1,2|numeric'
+    ];
+
+    public $translatable = [
+        'content'
     ];
 }
